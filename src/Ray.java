d20a6923edbf38f6204600747794e7e60e026f75
Ian <ian.hamilton117@gmail.com>
2013-11-16 20:04:47 -0700

    Messed with shadows, starting polygons

--- src/Ray.java
+++ src/Ray.java
@@ -9,7 +9,9 @@ public class Ray {
 	double near;
 	double far;
 	private static final int MAX_RGB_VALUE = 255;
+	private static final int DEFAULT_COLOR = 255;
 
+	// Constructor for use with rays from camera
 	public Ray(Vector prp, Vector pixel, double nearArg, double farArg) {
 		this.prp = prp;
 		direction = (pixel.minus(prp)).normalize();
@@ -19,10 +21,23 @@ public class Ray {
 		far = farArg;
 	}
 	
-	public void trace(Color color, Color depth) {
-		color.setAll(MAX_RGB_VALUE);		// Default color if no intersection is found
+	// Constructor for use with shadow checking rays
+	public Ray(Vector origin, Vector direction) {
+		this.direction = direction;
+		this.origin = origin;
+		prp = origin;
+		pixelToPrpDist = 0;
+		near = 0;
+		far = 0;
+	}
+	
+	public double trace(Color color, Color depth) {
+		color.setAll(DEFAULT_COLOR);		// Default color if no intersection is found
 		depth.setAll(0);		// Default depth if no intersection is found
-		double nearestIntersection = far;
+		double nearestSphereIntersection = -1;
+		Sphere sphereFinal = null;
+		double vFinal = 0;
+		double dFinal = 0;
 		for (int i = 0; i < RayTracer.spheres.size(); ++i) {
 			Sphere sphere = RayTracer.spheres.get(i);
 			Vector prpToSphereCenter = new Vector(sphere.center.minus(prp));
@@ -33,14 +48,20 @@ public class Ray {
 			if (b <= r) {
 				double d = Math.sqrt(r*r - b*b);
 				double intersection = v-d;
-				if (intersection >= pixelToPrpDist && intersection < nearestIntersection) {
-					nearestIntersection = intersection;
-					Vector Q = prp.plus(direction.times(v-d));  // World coordinates where ray intersects sphere
-					setColor(color, sphere, Q);
-					depth.setAll((int)Math.round(MAX_RGB_VALUE * (1 - (intersection-pixelToPrpDist)/(far-near))));	// Corrected for distance from prp to image plane
+				if (intersection >= pixelToPrpDist && (intersection < nearestSphereIntersection || nearestSphereIntersection == -1)) {
+					nearestSphereIntersection = intersection;
+					sphereFinal = sphere;
+					vFinal = v;
+					dFinal = d;
 				}
 			}
-		}		
+		}
+		if (nearestSphereIntersection != -1 && (near != 0 || far != 0)) {  // Don't enter if() if it's a shadow checker
+			Vector Q = prp.plus(direction.times(vFinal-dFinal));  // World coordinates where ray intersects sphere
+			setColor(color, sphereFinal, Q);
+			depth.setAll((int)Math.round(MAX_RGB_VALUE * (1 - (nearestSphereIntersection-pixelToPrpDist)/(far-near))));	// Corrected for distance from prp to image plane
+		}
+		return nearestSphereIntersection;
 	}
 	
 	private void setColor(Color sphereColor, Sphere sphere, Vector intersection) {
@@ -53,16 +74,24 @@ public class Ray {
 		Vector surfaceNormal = (intersection.minus(sphere.center)).normalize();  // N in slides
 		Vector directionToCamera = (prp.minus(intersection)).normalize();  // V in slides
 		for (int i=1; i<RayTracer.lights.size(); ++i) {
+			double distanceToLight = RayTracer.lights.get(i).position.minus(intersection).length();
 			Vector directionToLight = (RayTracer.lights.get(i).position.minus(intersection)).normalize();  // L in slides
-			Vector directionOfReflectedRay = ((surfaceNormal.times(2*(directionToLight.dot(surfaceNormal)))).minus(directionToLight)).normalize();  // R in slides
-			if (surfaceNormal.dot(directionToLight) > 0) {
-				diffuse = diffuse.add((RayTracer.lights.get(i).color.times(sphere.material.Kd)).times(surfaceNormal.dot(directionToLight)));
-			}
-			if (directionToCamera.dot(directionOfReflectedRay) > 0) {
-				specular = specular.add((RayTracer.lights.get(i).color.times(sphere.material.Ks)).times(Math.pow(directionToCamera.dot(directionOfReflectedRay), sphere.material.Ns)));
-			}
+//			Ray shadowCheck = new Ray(intersection, directionToLight);
+//			if (shadowCheck.trace(new Color(), new Color()) > distanceToLight) {
+				Vector directionOfReflectedRay = ((surfaceNormal.times(2*(directionToLight.dot(surfaceNormal)))).minus(directionToLight)).normalize();  // R in slides
+				if (surfaceNormal.dot(directionToLight) > 0) {
+					diffuse = diffuse.add((RayTracer.lights.get(i).color.times(sphere.material.Kd)).times(surfaceNormal.dot(directionToLight)));
+				}
+				if (directionToCamera.dot(directionOfReflectedRay) > 0) {
+					specular = specular.add((RayTracer.lights.get(i).color.times(sphere.material.Ks)).times(Math.pow(directionToCamera.dot(directionOfReflectedRay), sphere.material.Ns)));
+				}
+//			}
 		}
 		sphereColor.set(ambient.add(diffuse).add(specular));
 	}
+	
+/*	private double nearestIntersection() {
+		
+	}*/
 				
 }
