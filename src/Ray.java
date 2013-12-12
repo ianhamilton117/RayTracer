09245b31b0071c11bb86dc435b6e336d68de6ba4
ianhamilton117 <ian.hamilton117@gmail.com>
2013-12-11 22:24:04 -0700

    Temp for backtracking

--- src/Ray.java
+++ src/Ray.java
@@ -63,6 +63,7 @@ public class Ray {
 		}
 		
 		double nearestTriangleIntersection = -1;
+		double t = 0;
 		Face triangleFinal = null;
 		for (int i=0; i<RayTracer.groups.size(); ++i) {
 			Group group = RayTracer.groups.get(i);
@@ -70,7 +71,7 @@ public class Ray {
 				Face face = group.faces.get(j);
 				for (int k=0; k<face.triangles.size(); ++k) {
 					Face triangle = face.triangles.get(k);
-					double t = -((triangle.normal).dot(origin) + triangle.d) / (triangle.normal).dot(direction);
+					t = -((triangle.normal).dot(origin) + triangle.d) / (triangle.normal).dot(direction);
 					double[][] array1 = {{triangle.A.x, triangle.B.x, -direction.x},
 							             {triangle.A.y, triangle.B.y, -direction.y},
 							             {triangle.A.z, triangle.B.z, -direction.z}};
@@ -87,8 +88,7 @@ public class Ray {
 							if (intersection >= pixelToPrpDist && (intersection < nearestTriangleIntersection || nearestTriangleIntersection == -1)) {
 								nearestTriangleIntersection = intersection;
 								triangleFinal = triangle;
-								System.out.println("Poo");
-								color.setAll(150);
+//								color.setAll(150);
 							}
 						}
 					} catch (RuntimeException e) {
@@ -97,13 +97,23 @@ public class Ray {
 				}
 			}
 		}
-		
-		if (nearestSphereIntersection != -1 && (near != 0 || far != 0)) {  // Don't enter if() if it's a shadow checker
-			Vector Q = prp.plus(direction.times(vFinal-dFinal));  // World coordinates where ray intersects sphere
-			setColor(color, sphereFinal, Q);
-			depth.setAll((int)Math.round(MAX_RGB_VALUE * (1 - (nearestSphereIntersection-pixelToPrpDist)/(far-near))));	// Corrected for distance from prp to image plane
+		// If the ray hit a sphere
+		if (nearestSphereIntersection != -1 && (nearestTriangleIntersection == -1 || nearestSphereIntersection <= nearestTriangleIntersection)) {
+			if (near != 0 || far != 0) {  // Don't enter if() if it's a shadow checker
+				Vector Q = prp.plus(direction.times(vFinal-dFinal));  // World coordinates where ray intersects sphere
+				setColor(color, sphereFinal, Q);
+				depth.setAll((int)Math.round(MAX_RGB_VALUE * (1 - (nearestSphereIntersection-pixelToPrpDist)/(far-near))));	// Corrected for distance from prp to image plane
+			}
+			return nearestSphereIntersection;
 		}
-		return nearestSphereIntersection;
+		// If the ray hit a triangle
+		else if (nearestTriangleIntersection != -1 && (nearestSphereIntersection == -1 || nearestTriangleIntersection <= nearestSphereIntersection)) {
+			if (near != 0 || far != 0) {  // Don't enter if() if it's a shadow checker
+				Vector Q = prp.plus(direction.times(t));
+				color.setAll(0);
+			}
+		}
+		return -1;
 	}
 	
 	private void setColor(Color sphereColor, Sphere sphere, Vector intersection) {
@@ -115,6 +125,7 @@ public class Ray {
 		Color specular = new Color();
 		Vector surfaceNormal = (intersection.minus(sphere.center)).normalize();  // N in slides
 		Vector directionToCamera = (prp.minus(intersection)).normalize();  // V in slides
+		// TODO Implement lights at infinity
 		for (int i=1; i<RayTracer.lights.size(); ++i) {
 			double distanceToLight = RayTracer.lights.get(i).position.minus(intersection).length();
 			Vector directionToLight = (RayTracer.lights.get(i).position.minus(intersection)).normalize();  // L in slides
